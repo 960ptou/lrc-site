37af62f1be5eba9d426be676f5f9b61d15f83a2d
Weijie <wei@Weijies-Laptop.local>
2024-11-01 15:04:48 -0400

    basic audio component with sub

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse, FileResponse
+from lrc_utils import parse_lrc
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to LRC backend!"}
+
+@app.get("/lrc_file")
+async def lrc_file():
+    # Empty now
+    lrc_content = parse_lrc("")
+    return JSONResponse(content=lrc_content)
+    
+@app.get("/audio")
+async def get_audio():
+    return FileResponse("")
